7b0ad22acdd845a4334cedf36d98f25dfbab876a
Nikitarg1 <nikita.gomozov@gmail.com>
2020-10-07 14:42:44 +0300

    - попытка сделать паралакс

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import './assets/css/tailwind.css'
 import Vue from 'vue'
-import App from './App.vue'
 import vuetify from '@/plugins/vuetify'
+import App from './App.vue'
 import router from './router'
 
 
